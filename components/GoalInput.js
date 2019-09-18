f84379d6455878bacd6d59bbede2c8bb24440fc3
Vedant <mainvedant.vns123@gmail.com>
2019-09-18 15:19:42 +0530

    final commit

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -16,8 +16,14 @@ const GoalInput = props => {
     <Modal visible={props.visible} animationType="slide">
     <View style={styles.inputContainer}>
     <TextInput placeholder="Course Goal" onChangeText={goalInputHandler} style={styles.input} value={enteredGoal}/>
+    <View style={styles.buttonContainer}>
+    <View style={styles.button}>
     <Button title="add" onPress={addGoalhandler}></Button>
+    </View>
+    <View style={styles.button}>
    <Button title="cancel" onPress={props.close} color="red" /> 
+   </View>
+   </View>
   </View>
   </Modal>
     )
@@ -34,6 +40,14 @@ input:{
     borderWidth:1,
     padding:10,
     marginBottom:10
+},
+buttonContainer:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    width:'60%'
+},
+button:{
+    width:'40%'
 }
 })
 export default GoalInput
